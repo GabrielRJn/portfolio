ce75c37e948cf2a446a3696810f6e3f74ffeb43d
GabrielRJn <gabrielraianjohnson@gmail.com>
2023-12-29 19:09:28 +0000

    hover effects

--- src/components/Hero/Header/Header.jsx
+++ src/components/Hero/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, Image, Link, Spacer } from '@chakra-ui/react';
+import { Box, Flex, Text, Image, Link, Spacer, Tooltip } from '@chakra-ui/react';
 import github from '../../../assets/github.png';
 import gradcap from '../../../assets/gradcap.png';
 import linkedin from '../../../assets/linkedin.png';
@@ -18,15 +18,26 @@ const Header = () => {
       <Spacer/>
 
       <Box display="flex" opacity="60%" pr={10}>
-        <Link href="https://github.com/GabrielRJn" isExternal>
-        <Image src={github} alt="GitHub" boxSize={16} marginX={2} p={2}/>
-        </Link>
-        <Link href="https://www.aru.ac.uk/study/undergraduate/computer-science-beng/module-details?mo=5da17682-02cc-44d0-b983-ec74c8f6ce96&co=e0e61042-5970-4752-b089-debb5809eb96&cp=3b74f0d0-cd5f-485e-9343-fbe10f8f9998" isExternal>
-        <Image src={gradcap} alt="Graduation Cap" boxSize={16} marginX={2} p={2} />
-        </Link>
-        <Link href="https://www.linkedin.com/in/gabriel-johnson-a294951b9/">
-        <Image src={linkedin} alt="LinkedIn" boxSize={16} marginX={2} p={2} />
-        </Link>
+
+      <Tooltip label="GitHub" aria-label="GitHub" color="#FDC741" bg="#202020">
+  <Link href="https://github.com/GabrielRJn" isExternal>
+    <Image src={github} alt="GitHub" boxSize={16} marginX={2} p={2} className='github-icon hover-effect' />
+  </Link>
+</Tooltip>
+
+<Tooltip label="Degree information" aria-label="Degree information" color="#FDC741" bg="#202020">
+  <Link href="https://www.aru.ac.uk/study/undergraduate/computer-science-beng/module-details?mo=5da17682-02cc-44d0-b983-ec74c8f6ce96&co=e0e61042-5970-4752-b089-debb5809eb96&cp=3b74f0d0-cd5f-485e-9343-fbe10f8f9998" isExternal>
+    <Image src={gradcap} alt="Graduation Cap" boxSize={16} marginX={2} p={2} className="degree-icon hover-effect" />
+  </Link>
+</Tooltip>
+
+<Tooltip label="LinkedIn" aria-label="LinkedIn" color="#FDC741" bg="#202020">
+  <Link href="https://www.linkedin.com/in/gabriel-johnson-a294951b9/">
+    <Image src={linkedin} alt="LinkedIn" boxSize={16} marginX={2} p={2} className="linkedin-icon hover-effect" />
+  </Link>
+</Tooltip>
+
+
       </Box >
     </Flex>
   );
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,50 +1,99 @@
+import { motion } from "framer-motion";
 import { Box, Text, Heading, Flex, Link } from "@chakra-ui/react";
 import heroImage from "../../assets/heroImage.png";
-import 'typeface-lekton';
-import 'typeface-inter';
+import "typeface-lekton";
+import "typeface-inter";
+
+const MotionBox = motion(Box);
+const MotionText = motion(Text);
+const MotionHeading = motion(Heading);
+const MotionLink = motion(Link);
 
 export const Hero = () => {
+  const fadeInVariants = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 2 } },
+  };
+
   return (
     <Flex
       height="100vh"
       backgroundImage={`url(${heroImage})`}
-      backgroundSize="cover" 
-      backgroundPosition="center" 
+      backgroundSize="cover"
+      backgroundPosition="center"
       width="full"
+      overflow="hidden"
       className="hero"
     >
       <Flex
         m={24}
         opacity="100%"
-        direction={{ base: "column", md: "row" }} 
+        overflow="hidden"
+        direction={{ base: "column", md: "row" }}
         justify="space-between"
         align="center"
         width="full"
-        backgroundColor="#202020"
+        backgroundColor="#101010"
         borderRadius={50}
         p={10}
+        height="80%"
       >
-        <Box overflow="hidden">
-          <Heading color="white" pb={20} fontSize="6xl" >
+        <MotionBox
+          overflow="hidden"
+          initial="hidden"
+          animate="visible"
+          variants={fadeInVariants}
+        >
+          <MotionHeading color="white" pb={10} fontSize="8xl">
             Hi there!
-          </Heading>
-          <Text mb={10} color="white" fontSize="3xl" fontStyle="Inter" mr={5} fontWeight="bold">
+          </MotionHeading>
+          <MotionText
+            mb={8}
+            color="white"
+            fontSize="4xl"
+            fontStyle="Inter"
+            mr={5}
+            fontWeight="bold"
+          >
             My name is Gabriel Johnson. I'm a final year
-            <Text fontFamily="Lekton" as="span" color="#FDC741" ml={2} mr={2}>
+            <Text
+              fontFamily="Lekton"
+              as="span"
+              color="#FDC741"
+              ml={2}
+              mr={2}
+            >
               Computer Science student
             </Text>
             from Anglia Ruskin University.
-          </Text>
-          <Text fontSize={28} mt={5} opacity="50%" fontWeight="bold" color="white">
+          </MotionText>
+          <MotionText
+            fontSize={28}
+            mt={5}
+            opacity="50%"
+            fontWeight="bold"
+            color="white"
+          >
             Website made with:
-          </Text>
-        </Box>
+          </MotionText>
+        </MotionBox>
 
-        <Link display="flex" pl={20} fontSize={30} justifyContent="flex-end" color="white" fontWeight="bold">
+        <MotionLink
+          display="flex"
+          whiteSpace="nowrap"
+          mt={420}
+          mr={5}
+          fontSize="4xl"
+          justifyContent="flex-end"
+          color="white"
+          fontWeight="bold"
+          initial="hidden"
+          animate="visible"
+          variants={fadeInVariants}
+        >
           Learn More <Box as="span" color="#FDC741" ml={1}>&gt;&gt;</Box>
-        </Link>
+        </MotionLink>
       </Flex>
     </Flex>
   );
 };
-
