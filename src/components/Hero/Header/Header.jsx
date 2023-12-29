7d2e4cb7961f10f101248df4ad14676a090f3240
GabrielRJn <gabrielraianjohnson@gmail.com>
2023-12-29 03:33:42 +0000

    added icons

--- src/components/Hero/Header/Header.jsx
+++ src/components/Hero/Header/Header.jsx
@@ -1,15 +1,27 @@
+import { Box, Flex, Text, Image } from '@chakra-ui/react';
+import github from '../../../assets/github.png';
+import gradcap from '../../../assets/gradcap.png';
+import linkedin from '../../../assets/linkedin.png';
 
-import { Box, Text } from '@chakra-ui/react'
 const Header = () => {
   return (
-    <Box backgroundColor="white" height={50} flexDir="row">
-    <Text p={2}fontSize={24} color="#0C0C0C" opacity="60%" fontWeight="semibold">
+    <Flex
+      backgroundColor="white"
+      height={70}
+      alignItems="center" 
+      justifyContent="space-between"
+    >
+      <Text  fontSize={34} pl={28} color="#0C0C0C" opacity="60%" fontWeight="extrabold">
         Gabriel Johnson
-    </Text>
-    <Text color="white" display="flex" justifyContent="left" as='span'>Stuff</Text>
-    
-    </Box>
-  )
-}
+      </Text>
 
-export default Header
+      <Box display="flex" opacity="60%">
+        <Image src={github} alt="GitHub" boxSize="30px" marginX={2} />
+        <Image src={gradcap} alt="Graduation Cap" boxSize="30px" marginX={2} />
+        <Image src={linkedin} alt="LinkedIn" boxSize="30px" marginX={2} />
+      </Box>
+    </Flex>
+  );
+};
+
+export default Header;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,11 +13,13 @@ backgroundSize="cover"
 backgroundPosition="center" 
 width="full"
 height="818px"
+className="hero"
+opacity="95%"
 >
   
-      <Box m={24} width="80%" backgroundColor="#202020" borderRadius={10} p={5}>
+      <Box m={24} opacity="100%"  width="80%" backgroundColor="#202020" borderRadius={10} p={5}>
         <Heading color="white" fontSize="6xl" mb={3}>Hi there!</Heading>
-        <Text mb={5} color="white" fontSize="xl" fontStyle="Inter" fontWeight="bold" fontSize={27}>
+        <Text mb={5} color="white" fontSize="3xl" fontStyle="Inter" fontWeight="bold">
           My name is Gabriel Johnson. I'm a final year
          <Text fontFamily="Lekton" as="span" color="#FDC741" ml={2}>
            Computer Science student  </Text>
